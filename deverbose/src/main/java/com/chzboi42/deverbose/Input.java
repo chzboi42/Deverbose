a1a1312374b619c00cce8220d71dbf1c5d33f4e4
chzboi42 <1rajanjad@hdsb.ca>
2026-08-13 17:19:45 -0400

    Added newLine boolean to be a parameter for each method

--- deverbose/src/main/java/com/chzboi42/deverbose/Input.java
+++ deverbose/src/main/java/com/chzboi42/deverbose/Input.java
@@ -5,75 +5,61 @@ import java.util.Scanner;
 public class Input {
     private Input() {}
     private static final Scanner scanner = new Scanner(System.in);
-    private static boolean newLine = false;
-    private static final Input INSTANCE = new Input();
 
-    public static String inputString(String input) {
+    public static String inputString(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextLine();
     }
 
-    public static byte inputByte(String input) {
+    public static byte inputByte(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextByte();
     }
 
-    public static int inputInt(String input) {
+    public static int inputInt(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextInt();
     }
 
-    public static long inputLong(String input) {
+    public static long inputLong(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextLong();
     }
 
-    public static double inputDouble(String input) {
+    public static double inputDouble(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextDouble();
     }
 
-    public static float inputFloat(String input) {
+    public static float inputFloat(String input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.nextFloat();
     }
 
-    public static Object input(Object input) {
+    public static Object input(Object input, boolean newLine) {
         Console.print(input);
         if (newLine) {
             Console.write("");
-            newLine = false;
         }
         return scanner.next();
-    }
-
-    public static Input withNewLine() {
-        newLine = true;
-        return INSTANCE;
     }
 
 
